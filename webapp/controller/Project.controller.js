7e37c42c8a42b391cd386a946eef5f66e6f02400
AmelieNikeza <nikamelie09@gmail.com>
2023-12-11 14:25:04 +0100

    test

--- webapp/controller/Project.controller.js
+++ webapp/controller/Project.controller.js
@@ -17,7 +17,7 @@ sap.ui.define([
             },
 
             onPressSkills: function (oEvent) {
-                const sTest = "ed";
+                const sTeste = "ed";
                 const sMarche = "";
                 // Get clicked user path
                 var oBindingContext = oEvent.getSource().getBindingContext("data");
